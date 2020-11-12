e7e5106e989c519be9ba03a336ad3e740d78ac14
Hòa <lethanhhoa.boi@gmail.com>
2020-11-12 11:01:08 +0700

    update

--- ViewBasic/app/src/main/java/com/example/viewbasic/MainActivity.java
+++ ViewBasic/app/src/main/java/com/example/viewbasic/MainActivity.java
@@ -4,34 +4,112 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView imgViewPhoto;
+    ImageButton btnChangeImage, btnCloseImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         linkViews();
+        addEvents();
     }
 
     private void linkViews() {
         imgViewPhoto = findViewById(R.id.imgViewPhoto);
+        btnChangeImage = findViewById(R.id.btnChangeImage);
+        btnCloseImage = findViewById(R.id.btnCloseImage);
     }
 
-    public void changeImage(View view) {
-        if (imgViewPhoto.getTag() == null || imgViewPhoto.getTag().equals("android")) {
-            imgViewPhoto.setImageDrawable(getResources().getDrawable(R.drawable.ios));
-            imgViewPhoto.setTag("ios");
-        }
-        else {
-            imgViewPhoto.setImageDrawable(getResources().getDrawable(R.drawable.android));
-            imgViewPhoto.setTag("android");
+    // ----------- cach 4
+
+    private void addEvents() {
+        btnChangeImage.setOnClickListener(this);
+        btnCloseImage.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnChangeImage) {
+            if (imgViewPhoto.getTag() == null || imgViewPhoto.getTag().equals("android")) {
+                imgViewPhoto.setImageDrawable(getResources().getDrawable(R.drawable.ios));
+                imgViewPhoto.setTag("ios");
+            } else {
+                imgViewPhoto.setImageDrawable(getResources().getDrawable(R.drawable.android));
+                imgViewPhoto.setTag("android");
+            }
+        } else if (view.getId() == R.id.btnCloseImage) {
+            finish();
         }
     }
 
-    public void closeImage(View view) {
-        finish();
-    }
+
+    // ----------------- cach 3
+
+//    private void addEvents() {
+//        btnChangeImage.setOnClickListener(myClick);
+//        btnCloseImage.setOnClickListener(myClick);
+//    }
+//
+//    View.OnClickListener myClick = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            if (view.getId() == R.id.btnChangeImage) {
+//                if (imgViewPhoto.getTag() == null || imgViewPhoto.getTag().equals("android")) {
+//                    imgViewPhoto.setImageDrawable(getResources().getDrawable(R.drawable.ios));
+//                    imgViewPhoto.setTag("ios");
+//                } else {
+//                    imgViewPhoto.setImageDrawable(getResources().getDrawable(R.drawable.android));
+//                    imgViewPhoto.setTag("android");
+//                }
+//            } else if (view.getId() == R.id.btnCloseImage) {
+//                finish();
+//            }
+//        }
+//    };
+
+    // ------------ cach 2
+//    private void addEvents() {
+//        btnChangeImage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (imgViewPhoto.getTag() == null || imgViewPhoto.getTag().equals("android")) {
+//                    imgViewPhoto.setImageDrawable(getResources().getDrawable(R.drawable.ios));
+//                    imgViewPhoto.setTag("ios");
+//                } else {
+//                    imgViewPhoto.setImageDrawable(getResources().getDrawable(R.drawable.android));
+//                    imgViewPhoto.setTag("android");
+//                }
+//            }
+//        });
+//
+//        btnChangeImage.setOnLongClickListener(new View.OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View view) {
+//                Toast.makeText(MainActivity.this, "Hello my name is tofu", Toast.LENGTH_LONG).show();
+//                return false;
+//            }
+//        });
+//    }
+
+    // --------- cach 1 -------------
+
+//    public void changeImage(View view) {
+//        if (imgViewPhoto.getTag() == null || imgViewPhoto.getTag().equals("android")) {
+//            imgViewPhoto.setImageDrawable(getResources().getDrawable(R.drawable.ios));
+//            imgViewPhoto.setTag("ios");
+//        } else {
+//            imgViewPhoto.setImageDrawable(getResources().getDrawable(R.drawable.android));
+//            imgViewPhoto.setTag("android");
+//        }
+//    }
+
+//    public void closeImage(View view) {
+//        finish();
+//    }
 }
